ab318d28b8b7bdd73884f7a698f6752c464e5061
Nadirah Sofri <82518408+nadirahsofri@users.noreply.github.com>
2022-02-19 20:52:50 +0800

    Lesson 2.2 - ChatServer's ServerSocket and Socket

--- project02/ChatServer.java
+++ project02/ChatServer.java
@@ -1,5 +1,16 @@
 package project02;
-
+import java.net.ServerSocket;
+import java.net.BindException;
+import java.io.IOException;
 public class ChatServer {
-
+	public static void main(String[]args) {
+		ServerSocket serversocket = null;
+		try {
+			serversocket = new ServerSocket(9101);
+		} catch (BindException e) {
+			System.out.println("Unable to start server due to port is already in used.");
+		} catch (IOException e) {
+			System.out.println("Unexpected problem. Unable to start server.");
+		}
+	}
 }
