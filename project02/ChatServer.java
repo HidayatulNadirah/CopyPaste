6163fb9042c1a169812689f8ed122c4e52fe7623
Nadirah Sofri <82518408+nadirahsofri@users.noreply.github.com>
2022-02-19 21:06:44 +0800

    Lesson 2.3 - SessionInitial

--- project02/ChatServer.java
+++ project02/ChatServer.java
@@ -2,15 +2,30 @@ package project02;
 import java.net.ServerSocket;
 import java.net.BindException;
 import java.io.IOException;
+import java.util.Date;
+import java.net.Socket;
 public class ChatServer {
 	public static void main(String[]args) {
-		ServerSocket serversocket = null;
+		ServerSocket serverSocket = null;
 		try {
-			serversocket = new ServerSocket(9101);
+			serverSocket = new ServerSocket(9101);
+			System.out.println("ServerSocket at " + new Date());
+			while(true) {
+				 Socket client = serverSocket.accept();
+				 new Thread(new SessionInitial(client)).start();
+				 }
 		} catch (BindException e) {
 			System.out.println("Unable to start server due to port is already in used.");
 		} catch (IOException e) {
 			System.out.println("Unexpected problem. Unable to start server.");
+		}finally {
+			try {
+				if (serverSocket !=null) {
+					serverSocket.close();
+				}
+			}catch (IOException e) {
+				System.out.println("Unexpected problem. Unable to close server socket.");
+			}
 		}
 	}
 }
--- project02/SessionInitial.java
+++ project02/SessionInitial.java
@@ -1,5 +1,31 @@
 package project02;
+import java.net.Socket;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 
-public class SessionInitial {
-
+public class SessionInitial implements Runnable{
+		private Socket client;
+		 private DataInputStream fromClient;
+		 private DataOutputStream toClient;
+		
+		public SessionInitial(Socket client) {
+			this.client = client;
+			System.out.println("Client connected: " +
+			client.getInetAddress().getHostAddress() + " " 
+			+ client.getInetAddress().getHostName() + " .");
+	}
+		
+		public void run() {
+			try {
+			fromClient = new DataInputStream(client.getInputStream());
+			toClient = new DataOutputStream(client.getOutputStream());
+			
+			toClient.writeUTF("Welcome to PB Chatting");
+			} catch (IOException e) {
+				System.out.println("Communication problem with " +
+		client.getInetAddress().getHostAddress()
+				 + " " + client.getInetAddress().getHostName() + " .");
+		}
+	}
 }
