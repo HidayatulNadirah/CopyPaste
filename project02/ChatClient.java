a164196a03a36e0eab3a4a2a52349c4a12c92505
Nadirah Sofri <82518408+nadirahsofri@users.noreply.github.com>
2022-02-20 11:37:58 +0800

    Lesson 2.5 - Main Menu

--- project02/ChatClient.java
+++ project02/ChatClient.java
@@ -12,40 +12,73 @@ public class ChatClient {
 	private static String ipAddress = "localhost";
 	private static int portNumber = 9101;
 	private static Socket socket;
-		private static DataInputStream fromServer;
-		private static DataOutputStream toServer;
-		private static Scanner scanner;
-		private static boolean chatActive = true;
-		
+	private static DataInputStream fromServer;
+	private static DataOutputStream toServer;
+	private static Scanner scanner;
+	private static boolean chatActive = true;
+
 	public static void main(String[]args) {
 		try {
-			 socket = new Socket(ipAddress, portNumber);
-			 
-			 fromServer = new DataInputStream(socket.getInputStream());
-			 
-			 toServer = new DataOutputStream(socket.getOutputStream());
-			 
-			 scanner = new Scanner(System.in);
-			 
-			 System.out.println(fromServer.readUTF());
-			 
-			 } catch (ConnectException | UnknownHostException e) {
-			 System.out.println("Unable to connect to server " + ipAddress + ":" +
-			portNumber);
-			 } catch (IOException e) {
-			 System.out.println("Problem communicating with the server.");
-			 } finally {
-			 try {
-			 if(socket != null) {
-			 socket.close();
-			 }
-			 
-			 if(fromServer != null) {
-			 fromServer.close();
-			 }
-			 if(toServer != null) {
-			 toServer.close();
-			 }}
-			 catch (IOException e) {}
-			 }}}
-  
+			socket = new Socket(ipAddress, portNumber);
+
+			fromServer = new DataInputStream(socket.getInputStream());
+
+			toServer = new DataOutputStream(socket.getOutputStream());
+
+			scanner = new Scanner(System.in);
+
+			System.out.println(fromServer.readUTF());
+			mainMenuLogic();
+
+		} catch (ConnectException | UnknownHostException e) {
+			System.out.println("Unable to connect to server " + ipAddress + ":" +
+					portNumber);
+		} catch (IOException e) {
+			System.out.println("Problem communicating with the server.");
+		} finally {
+			try {
+				if(socket != null) {
+					socket.close();
+				}
+
+				if(fromServer != null) {
+					fromServer.close();
+				}
+				if(toServer != null) {
+					toServer.close();
+				}}
+			catch (IOException e) {}
+		}}
+
+	private static void mainMenuLogic() throws IOException{
+		readMainMenu();
+		selectMainMenuOption();
+	}
+
+	private static void readMainMenu() throws IOException {
+		System.out.println(fromServer.readUTF());
+	}
+
+	public static void selectMainMenuOption() throws IOException{
+		String option = scanner.nextLine();
+		toServer.writeUTF(option);
+
+		boolean validOptionSelected = fromServer.readBoolean();
+		if(validOptionSelected) {
+			if(option.equals("1")) {
+				waitingForOtherClient();
+			}
+			if(option.equals("2")) {
+				listDownAvailableSession();
+			}
+		}else {
+			System.out.println(fromServer.readUTF());
+			mainMenuLogic();
+		}
+	}
+	public static void waitingForOtherClient() {
+	}
+	public static void listDownAvailableSession() {
+	}
+}
+
--- project02/SessionInitial.java
+++ project02/SessionInitial.java
@@ -1,31 +1,76 @@
 package project02;
+import java.util.*;
+import java.io.*;
 import java.net.Socket;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class SessionInitial implements Runnable{
-		private Socket client;
-		 private DataInputStream fromClient;
-		 private DataOutputStream toClient;
-		
-		public SessionInitial(Socket client) {
-			this.client = client;
-			System.out.println("Client connected: " +
-			client.getInetAddress().getHostAddress() + " " 
-			+ client.getInetAddress().getHostName() + " .");
+	private Socket client;
+	private DataInputStream fromClient;
+	private DataOutputStream toClient;
+
+	public void sendMainMenu() throws IOException {
+		String menu = "";
+		menu = menu + "Please choose an option:\n";
+		menu = menu + "(1) Start a new Chatting session\n";
+		menu = menu + "(2) Connect to available session\n";
+		toClient.writeUTF(menu);
 	}
-		
-		public void run() {
-			try {
+
+	public SessionInitial(Socket client) {
+		this.client = client;
+		System.out.println("Client connected: " +
+				client.getInetAddress().getHostAddress() + " " 
+				+ client.getInetAddress().getHostName() + " .");
+	}
+
+	public void run() {
+		try {
 			fromClient = new DataInputStream(client.getInputStream());
 			toClient = new DataOutputStream(client.getOutputStream());
-			
+
 			toClient.writeUTF("Welcome to PB Chatting");
-			} catch (IOException e) {
-				System.out.println("Communication problem with " +
-		client.getInetAddress().getHostAddress()
-				 + " " + client.getInetAddress().getHostName() + " .");
+			mainMenuLogic();
+
+		} catch (IOException e) {
+			System.out.println("Communication problem with " +
+					client.getInetAddress().getHostAddress()
+					+ " " + client.getInetAddress().getHostName() + " .");
 		}
+
 	}
+	public void mainMenuLogic() throws IOException{
+		sendMainMenu();
+		readOptionFromClient();
+	}
+	public void readOptionFromClient() throws IOException{
+		String option = fromClient.readUTF();
+
+		boolean validOptionSelected = false;
+		if (option.equals("1") || option.equals("2")) {
+			validOptionSelected = true;
+			toClient.writeBoolean(validOptionSelected);
+			if(option.equals("1")) {
+				startANewSession();
+			}if(option.equals("2")) {
+				listDownAvailableSession();
+			}
+		}else {
+			validOptionSelected = false;
+			toClient.writeBoolean(validOptionSelected);
+			toClient.writeUTF("Wrong option. Please select correct option.");
+			mainMenuLogic();
+		}
+
+	}
+	public void startANewSession() {
+
+	}
+
+	public void listDownAvailableSession() {
+
+	}
+
 }
